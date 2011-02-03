a91be46170811b7ae6ddb4a2524f6046cca16db1
Toyokazu Akiyama <toyokazu@gmail.com>
2011-02-03 20:11:38 +0900

    fix prototype.ujs related things.
    - Transaction handling
    - Ajax update message

--- app/controllers/wifi_logs_controller.rb
+++ app/controllers/wifi_logs_controller.rb
@@ -20,55 +20,52 @@ class WifiLogsController < ApplicationController
   # POST /maps/1/wifi_logs
   # POST /maps/1/wifi_logs
   def create
-    WifiLog.transaction do
-      @manual_location = ManualLocation.new
-      @manual_location.init_by_params(params)
-      @manual_location.save!
+    begin
+      WifiLog.transaction do
+        @manual_location = ManualLocation.new
+        @manual_location.init_by_params(params)
+        @manual_location.save!
 
-      @gpsd_tpv = nil
-      @gps_location = nil
-      if !params["gpsd_tpv"].nil?
-        @gpsd_tpv = JSON.parse(params["gpsd_tpv"])
-        if !@gpsd_tpv.empty?
-          @gps_location = GpsLocation.new_by_json(@gpsd_tpv)
-          @gps_location.save!
+        @gpsd_tpv = nil
+        @gps_location = nil
+        if !params["gpsd_tpv"].nil? && !params["gpsd_tpv"].empty?
+          @gpsd_tpv = JSON.parse(params["gpsd_tpv"])
+          if !@gpsd_tpv.empty?
+            @gps_location = GpsLocation.new_by_json(@gpsd_tpv)
+            @gps_location.save!
+          end
+        end
+        @movement_log = MovementLog.new
+        @movement_log.manual_location = @manual_location
+        @movement_log.gps_location = @gps_location
+        # FIXME
+        # how to specify current user in Devise
+        @movement_log.user = current_user
+        @movement_log.remote_addr = request.remote_addr
+        @movement_log.user_agent = request.user_agent
+        @movement_log.user_device = params["user_device"]
+        @movement_log.save!
+
+        if !params["wifi_towers"].nil? && !params["wifi_towers"].empty?
+          @wifi_towers = JSON.parse(params["wifi_towers"])
+          @wifi_towers.each do |wifi_tower|
+            @wifi_access_point = WifiAccessPoint.find_or_initialize_by_json(wifi_tower)
+            if !@wifi_access_point.persisted?
+              @wifi_access_point.save!
+            end
+            @wifi_log = WifiLog.new_by_json(wifi_tower)
+            @wifi_log.wifi_access_point = @wifi_access_point
+            @wifi_log.movement_log = @movement_log
+            @wifi_log.save!
+          end
         end
       end
-      @movement_log = MovementLog.new
-      @movement_log.manual_location = @manual_location
-      @movement_log.gps_location = @gps_location
-      # FIXME
-      # how to specify current user in Devise
-      @movement_log.user = current_user
-      @movement_log.remote_addr = request.remote_addr
-      @movement_log.user_agent = request.user_agent
-      @movement_log.user_device = params["user_device"]
-
-      @wifi_towers = JSON.parse(params["wifi_towers"])
-      @wifi_towers.each do |wifi_tower|
-        @wifi_access_point = WifiAccessPoint.find_or_initialize_by_json(wifi_tower)
-        if !@wifi_access_point.persisted?
-          @wifi_access_point.save!
-        end
-        @wifi_log = WifiLog.new_by_json(wifi_tower)
-        @wifi_log.wifi_access_point = @wifi_access_point
-        @wifi_log.movement_log = @movement_log
-        @wifi_log.save!
+      respond_to do |format|
+        format.html { render :text => "Logs are successfully recorded." }
       end
-    end
-
-    # FIXME
-    # how to detect transaction rollback in transaction method
-    # set flag in transaction method then process as follows
-    # this method is called by JavaScript XMLHttpRequest
-    # so thus json reply may be adequate
-    respond_to do |format|
-      if @map.save
-        format.html { redirect_to(@map, :notice => 'Map was successfully created.') }
-        format.xml  { render :xml => @map, :status => :created, :location => @map }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @map.errors, :status => :unprocessable_entity }
+    rescue
+      respond_to do |format|
+        format.html { render :text => "Failed to record logs." }
       end
     end
   end
